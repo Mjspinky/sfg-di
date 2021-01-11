1202b9a38ef5eb65cb69783f924682648f33da6c
Mjspinky <Matthewpinkerton@hotmail.co.uk>
2021-01-11 10:46:30 +0000

    Before Assignment 4

---
+++ src/main/java/guru/springframework/sfgdi/controllers/myController.java
@@ -0,0 +1,20 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class myController {
+
+
+    private final GreetingService greetingService;
+
+    public myController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello(){
+
+        return greetingService.sayGreeting();
+    }
+}
